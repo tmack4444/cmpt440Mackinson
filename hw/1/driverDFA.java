210bd4297fee049a97ddd2e6cf0d5d94d08fc70b
tmack4444 <macki4444@gmail.com>
2019-03-06 10:59:28 -0500

    Started working on the DFA driver, defined the table of states, based on my spreadsheet of state transitions

---
+++ hw/1/driverDFA.java
@@ -0,0 +1,18 @@
+class dfa{
+    public static void main(String[] args) {
+       public static int[] [] delta = {
+         /*q0*/ {q10, q1, q10, q10},
+         /*q1*/ {q10, q0, q10, q2},
+         /*q2*/ {q4, q10, q3, q1},
+         /*q3*/ {q10, q5, q2, q10},
+         /*q4*/ {q2, q6, q10, q10},
+         /*q5*/ {q7, q3, q10, q10},
+         /*q6*/ {q10, q4, q7, q10},
+         /*q7*/ {q5, q10, q6, q8},
+         /*q8*/ {q10, q9, q10, q7},
+         /*q9*/ {q10, q8, q10, q10},
+         /*q10*/ {q10, q10, q10, q10}
+       };
+            
+    }
+}
