4f95408f290dca9f07a2a2fd21dcdc14bc8f09f6
unknown <thomas.mackinson1@marist.edu>
2019-04-24 10:26:54 -0400

    Got the project file working. This is shorter than homework 1 is, so if you have any comments I would like to hear them soon since I still have enough time before the due date to make some small additions

--- hw/prj/driverDFA.java
+++ hw/prj/driverDFA.java
@@ -4,6 +4,6 @@ public class driverDFA{
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
        String inputText = scanner.nextLine();
-       ManWolf.process(inputText);
+       TrafficLight.process(inputText);
      }
 }
